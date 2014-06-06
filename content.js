08479c6e894e2b0d70c573e549bc6c9616a15b80
shivamd <daryananis@gmail.com>
2014-06-06 09:29:11 -0700

    Restructure code for an easier read

--- content.js
+++ content.js
@@ -1,3 +1,33 @@
+$(document).ready(function(){
+  if (isUserViewingGemfile()){
+    addGemToolTip();
+  } 
+  if (location.origin.match(/github/)){
+    setInterval(function(){
+      if (isUserViewingGemfile()){
+        addGemToolTip();
+      }
+    },
+    3000);
+  }
+});
+
+var isUserViewingGemfile = function(){
+  return !!(location.origin.match(/github/) && location.pathname.match(/Gemfile/));
+}
+
+var addGemToolTip = function(){
+  var matches = $("span:contains('gem')")
+  for (i=0; i < matches.length; i++){
+    function getGemName(){
+      return $(matches[i]).siblings("span").first().text().replace(/('|")/g, "")
+    }
+    var gemName = getGemName();
+    markupForAnnotationContainer($(matches[i]), gemName);
+    hoverEventForGemContainer($(matches[i]).parent(), gemName)
+  }
+};
+
 var markupForAnnotationContainer = function($selector, gemName){
   $selector.parent().append("<div id='"+gemName+"'></div>")
   $selector.parent().css("position", "relative")
@@ -18,6 +48,46 @@ var markupForAnnotationContainer = function($selector, gemName){
     "font-size": "10px",
     "padding": "0 4px"
   })
+}
+
+var hoverEventForGemContainer = function($container, gemName){
+  $container.on({
+    mouseenter: function(){
+      $("#"+gemName).css("display", "block")
+      if ($("#"+gemName).children().length == 0) {
+        getGemInfo($container, gemName);
+      }
+    },
+
+    mouseleave: function(){
+      $("#"+gemName).css("display", "none")
+    }
+  });
+}
+
+var getGemInfo = function($container, gemName){
+  $.ajax({
+    url: "https://rubygems.org/api/v1/gems/"+gemName+".json",
+    type: "GET",
+  }).done(function(data){
+    var gemDescription = data.info
+    var projectURL = data.homepage_uri
+    addMarkupForGemInformation(gemName, gemDescription, projectURL);
+  }).error(function(){
+    console.log("error");
+  })
+}
+
+var addMarkupForGemInformation = function(gemName, gemDescription, projectURL){
+  var gemInformationMarkup = "<p class='info-container'><a class='gem-link' target='_blank' href='"+projectURL+"'><span class='arrow-left'></span><span class='gem-info'>"+gemDescription.substring(0,150)+"</span> <br/><span class='project-link'>Project Link</span></a></p>"
+  $("#"+gemName).html(gemInformationMarkup);
+  setStyles()
+};
+
+var setStyles = function(){
+  setStyleForInfoContainer();
+  setStyleForLinkToProject()
+  setStyleForArrow();
 }
 
 var setStyleForInfoContainer = function(){
@@ -52,68 +122,3 @@ var setStyleForArrow = function(){
   })
 }
 
-var setStyles = function(){
-  setStyleForInfoContainer();
-  setStyleForLinkToProject()
-  setStyleForArrow();
-}
-
-var addMarkupForGemInformation = function(gemName, gemDescription, projectURL){
-  var gemInformationMarkup = "<p class='info-container'><a class='gem-link' target='_blank' href='"+projectURL+"'><span class='arrow-left'></span><span class='gem-info'>"+gemDescription.substring(0,150)+"</span> <br/><span class='project-link'>Project Link</span></a></p>"
-  $("#"+gemName).html(gemInformationMarkup);
-  setStyles()
-};
-
-var getGemInfo = function($container, gemName){
-  $.ajax({
-    url: "https://rubygems.org/api/v1/gems/"+gemName+".json",
-    type: "GET",
-  }).done(function(data){
-    var gemDescription = data.info
-    var projectURL = data.homepage_uri
-    addMarkupForGemInformation(gemName, gemDescription, projectURL);
-  }).error(function(){
-    console.log("error");
-  })
-}
-var hoverEventForGemContainer = function($container, gemName){
-  $container.on({
-    mouseenter: function(){
-      $("#"+gemName).css("display", "block")
-      if ($("#"+gemName).children().length == 0) {
-        getGemInfo($container, gemName);
-      }
-    },
-
-    mouseleave: function(){
-      $("#"+gemName).css("display", "none")
-    }
-  });
-}
-var addGemToolTip = function(){
-  var matches = $("span:contains('gem')")
-  for (i=0; i < matches.length; i++){
-    function getGemName(){
-      return $(matches[i]).siblings("span").first().text().replace(/('|")/g, "")
-    }
-    var gemName = getGemName();
-    markupForAnnotationContainer($(matches[i]), gemName);
-    hoverEventForGemContainer($(matches[i]).parent(), gemName)
-  }
-};
-var isUserViewingGemfile = function(){
-  return !!(location.origin.match(/github/) && location.pathname.match(/Gemfile/));
-}
-$(document).ready(function(){
-  if (isUserViewingGemfile()){
-    addGemToolTip();
-  } 
-  if (location.origin.match(/github/)){
-    setInterval(function(){
-      if (isUserViewingGemfile()){
-        addGemToolTip();
-      }
-    },
-    3000);
-  }
-});
